a342fefb575eefb7571a927596480b9b80f0e12d
serpilkuzu <serpilkuzuu@gmail.com>
2021-01-10 01:41:07 +0300

    App init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './nehir.jpeg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,34 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Serpil Kuzu
         </p>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://www.linkedin.com/in/serpilkuzu/"
+            target="_blank"
+            rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
+        </a>
+
+        <a
+            className="App-link"
+            href="https://github.com/serpilkuzu"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+
+        <a
+            className="App-link"
+            href="https://www.instagram.com/kuzu.serpil/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Instagram
         </a>
       </header>
     </div>
